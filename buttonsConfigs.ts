4eff6bb98ba2d5eb990f17447d3f7da6c8ffc4fa
justame <yaronn@wix.com>
2022-01-08 01:49:58 +0200

    refactor

--- buttonsConfigs.ts
+++ buttonsConfigs.ts
@@ -1,8 +1,8 @@
 import {
   ToolbarItemConfig,
   IToolbarItem,
-  ContentResolver,
   ToolbarSpec,
+  ContentResolver,
 } from './types';
 export const configs: ToolbarItemConfig[] = [
   {
@@ -69,31 +69,54 @@ export class ToolbarItem implements IToolbarItem {
 
 export const createToolbarItemByConfig = (config: ToolbarItemConfig) => {
   const toolbarItem = ToolbarItem.create(config.id, config.label);
+  // default values
   Object.keys(config.attributes).forEach((attributeName) => {
     const defaultValue = config.attributes[attributeName].defaultValue;
     toolbarItem.setAttribute(attributeName, defaultValue);
   });
 
-  return toolbarItem;
+  const updateAttributes = (content: Content) => {
+    Object.keys(config.attributes).forEach((attributeName) => {
+      const value = content.resolve(config.attributes[attributeName]);
+      toolbarItem.setAttribute(attributeName, value);
+    });
+  };
+  return { toolbarItem, updateAttributes };
 };
 
-export const resolveAttributes = (
-  contentResolver: ContentResolver<any>,
-  content
-) => {
-  return contentResolver.resolve(content);
-};
+export class Content {
+  private constructor(private content) {}
+  private resolved = {};
+  resolve(contentResolver: ContentResolver<any>) {
+    if (this.resolved[contentResolver.id]) {
+      return this.resolved[contentResolver.id];
+    } else {
+      this.resolved[contentResolver.id] = contentResolver.resolve(this.content);
+    }
+    return this.resolved[contentResolver.id];
+  }
+  static create(content) {
+    return new Content(content);
+  }
+}
 
 export class Toolbar {
-  private constructor(readonly toolbarItems: IToolbarItem[]) {
+  id: string;
+  readonly toolbarItems: IToolbarItem[] = [];
+  private constructor() {
     // read about private constructor
   }
 
-  static create(toolbarItems: IToolbarItem[]) {
-    return new Toolbar(toolbarItems);
+  static create() {
+    return new Toolbar();
   }
 
   getItemsBy(spec: ToolbarSpec) {
     return this.toolbarItems.filter(spec);
+  }
+
+  addToolbarItem(toolbarItem: IToolbarItem) {
+    // validate no duplicate id
+    this.toolbarItems.push(toolbarItem);
   }
 }
--- index.tsx
+++ index.tsx
@@ -1,41 +1,32 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
 import './style.css';
 import ToolbarComponent from './Toolbar';
-// import content from './content.json';
 import { Tiptap } from './TiptapEditor';
-import { IContentSpec } from './types';
 
 import {
   createToolbarItemByConfig,
   configs,
   Toolbar as ToolbarManager,
   Toolbar,
+  Content,
 } from './buttonsConfigs';
 
 interface AppProps {}
 interface AppState {
-  name: string;
-  selectedNodeIndex: number;
   selectedContent: any;
-  specs: IContentSpec[];
 }
-
-const buttons = [];
 
 class App extends Component<AppProps, AppState> {
   toolbarItemIdAttributesMap = {};
   toolbarItems = [];
   toolbar: Toolbar = null;
+  updateAttributesList = [];
 
   constructor(props) {
     super(props);
     this.state = {
-      name: 'React',
-      selectedNodeIndex: 1,
       selectedContent: null,
-      specs: [],
     };
   }
 
@@ -46,45 +37,29 @@ class App extends Component<AppProps, AppState> {
   setSelection = (nodes) => {
     console.log('setSelection', nodes);
     this.setState({ selectedContent: nodes });
-    this.toolbarItems.forEach((toolbarItem) => {
-      const attributes = this.toolbarItemIdAttributesMap[toolbarItem.id];
-      Object.keys(attributes).forEach((attributeName) => {
-        const resolvedValue = attributes[attributeName].resolve(nodes);
-        toolbarItem.setAttribute(attributeName, resolvedValue);
-      });
+    const content = Content.create(nodes);
+    this.updateAttributesList.forEach((updateAttribute) => {
+      updateAttribute(content);
     });
   };
 
   componentWillMount() {
-    const toolbarItemIdAttributesMap = {};
-    const toolbarItems = [];
+    this.toolbar = Toolbar.create();
+
+    // const toolbarItemIdAttributesMap = {};
+    // const toolbarItems = [];
     configs.forEach((config) => {
-      const toolbarItem = createToolbarItemByConfig(config);
-      toolbarItemIdAttributesMap[toolbarItem.id] = config.attributes;
-      toolbarItems.push(toolbarItem);
+      const { toolbarItem, updateAttributes } =
+        createToolbarItemByConfig(config);
+      this.updateAttributesList.push(updateAttributes);
+      this.toolbar.addToolbarItem(toolbarItem);
     });
-
-    this.toolbarItemIdAttributesMap = toolbarItemIdAttributesMap;
-    this.toolbarItems = toolbarItems;
-
-    this.toolbar = Toolbar.create(toolbarItems);
   }
 
   render() {
     return (
       <div>
         <ToolbarComponent toolbar={this.toolbar} />
-        <div>
-          <button
-            onClick={() =>
-              this.setState({
-                selectedNodeIndex: this.state.selectedNodeIndex + 1,
-              })
-            }
-          >
-            next node
-          </button>
-        </div>
         <div style={{ height: 100, overflow: 'auto' }}>
           {this.renderNodeContent()}
         </div>
